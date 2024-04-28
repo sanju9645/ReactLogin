80e44e7bb5ec01d81c6a29eee03639636e07f0ec
Sanju S Nath <sanju@nethram.com>
2024-04-28 21:48:05 +0530

    Hotfix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+    <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12" style={{ maxHeight: '100vh' }}>
     <div className="relative py-3 sm:max-w-xl sm:mx-auto">
       <div
         className="absolute inset-0 bg-gradient-to-r from-slate-900 to-slate-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl">
